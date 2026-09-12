9d007954e5ddaee16737f8d0d9eee16cab2245f6
krutoi322 <SemMisha0808@mail.ru>
2026-09-12 10:08:37 +0300

    Update logger.js

--- logger.js
+++ logger.js
@@ -4,17 +4,17 @@ const filepath = path.join(__dirname, 'logs.txt');
 const timenow = new Date().toLocaleTimeString();
 const stream = fs.createWriteStream(filepath, { flags: 'a', encoding: 'utf8' });
 function setupLogger(server) {
-    server.once('ServerWithPortCreated', (port) => {
+    server.once('server:started', (port) => {
         stream.write(`[${timenow}] ServerWithPortCreated: localhost: ${port}\n`)
     });
 
-    server.once('serverclosed', () => {
+    server.once('server:stopped', () => {
         console.log("сервер отключен\n")
         stream.write(`[${timenow}] serverclosed: Сервер отключен\n`)
         process.exit(0);
     });
 
-    server.on('z', (url, method) => {
+    server.on('server:received', (url, method) => {
         stream.write(`[${timenow}] z : ${url} ${method}\n`)
     });
 }
